4a3930ffaaa227ecfb31a0eb16f547c5b5a1130c
ivangladius <112496431+ivangladius@users.noreply.github.com>
2023-03-17 19:01:47 +0100

    Add files via upload

---
+++ custom.el
@@ -0,0 +1,12 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(disaster-objdump "objdump -d -M intel -Sl --no-show-raw-insn"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :foreground "red" :height 2.0)))))
